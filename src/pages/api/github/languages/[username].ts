b4ed59de86213c20a0e0d550d0f02cef215f72f4
Alex Oliveira <alexoliveira7x@gmail.com>
2023-10-04 00:01:27 +0100

    chore: change header

--- src/pages/api/github/languages/[username].ts
+++ src/pages/api/github/languages/[username].ts
@@ -1,26 +1,27 @@
-import { JSDOM } from 'jsdom';
+import { JSDOM } from "jsdom";
 
 const getContent = async (name: string) => {
-	const url = 'https://git-generator.vercel.app/github/languages/' + name;
+  const url = "https://git-generator.vercel.app/github/languages/" + name;
 
-	return fetch(url)
-		.then((response) => response.text())
-		.then((html) => {
-			const dom = new JSDOM(html);
-			return dom.window.document.querySelector('main')?.innerHTML ?? '';
-		})
-		.catch(() => {
-			return '';
-		});
+  return fetch(url)
+    .then((response) => response.text())
+    .then((html) => {
+      const dom = new JSDOM(html);
+      return dom.window.document.querySelector("main")?.innerHTML ?? "";
+    })
+    .catch(() => {
+      return "";
+    });
 };
 
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse<string>
+  req: NextApiRequest,
+  res: NextApiResponse<string>,
 ) {
-	res.setHeader('Content-Type', 'svg+xml');
-	const content = await getContent(req.query.username as string);
-	res.status(200).send(content);
+  res.setHeader("Content-Type", "image/svg+xml");
+
+  const content = await getContent(req.query.username as string);
+  res.status(200).send(content);
 }
